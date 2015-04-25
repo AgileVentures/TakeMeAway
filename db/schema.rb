8923949db1e6360e28233c4e654c3df256fad46b
Raoul DIFFOUO <diraulo@gmail.com>
2015-04-25 21:33:19 +0200

    remove email field from user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420210521) do
+ActiveRecord::Schema.define(version: 20150425193211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20150420210521) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
     t.boolean  "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
