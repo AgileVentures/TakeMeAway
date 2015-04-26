9236d813534f1f3270f6a9e16a9f16c479426213
tochman <thomas@nocebo.se>
2015-04-26 11:18:53 +0200

    refactored factory for user and added scopes for 'clients' & 'admins' to model. Added scope to User view in AA

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,26 +1,31 @@
 ActiveAdmin.register User do
+  scope :clients
+  scope :admins
+
   permit_params :email, :password, :password_confirmation
+
 
   index do
     selectable_column
-    id_column
+    #id_column
+    column :name
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column 'Member since', :created_at
     actions
   end
 
+  filter :name
   filter :email
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
 
   form do |f|
-    f.inputs "Admin Details" do
+    f.inputs "User Details" do
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :is_admin
     end
     f.actions
   end
