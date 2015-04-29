c00bb6ca1440217ba9788e69f32f02193e1f74be
tochman <thomas@nocebo.se>
2015-04-29 17:31:43 +0200

    added 'session_spec'. also, minor refactorings

--- spec/requests/api/v1/menu_spec.rb
+++ spec/requests/api/v1/menu_spec.rb
@@ -1,31 +1,27 @@
 require 'rails_helper'
 
-# create_table "menus", force: :cascade do |t|
-#   t.boolean  "show_category"
-#   t.datetime "start_date"
-#   t.datetime "end_date"
-#   t.datetime "created_at",    null: false
-#   t.datetime "updated_at",    null: false
+describe Api::V1::MenusController do
+  describe 'GET /v1/menus/:id' do
 
-describe 'GET /v1/menus/:id' do
+    before do
+      @menu = FactoryGirl.create(:menu)
+    end
 
-  before do
-    @menu = FactoryGirl.create(:menu)
-  end
+    it 'returns a menu by :id' do
+      menu = @menu
+      get "/v1/menus/#{menu.id}"
+      expect(response_json).to eq({'menu' =>
+                                       {
+                                           'start_date' => menu.start_date.strftime('%F'),
+                                           'end_date' => menu.end_date.strftime('%F')
+                                       },
+                                   'specials' =>
+                                       {
+                                           'items' => 'no items'
+                                       }
 
-  it 'returns a menu by :id' do
-    menu = @menu
-    get "/v1/menus/#{menu.id}"
-    expect(response_json).to eq({'menu' =>
-                                     {
-                                         'start_date' => menu.start_date.strftime('%F'),
-                                         'end_date' => menu.end_date.strftime('%F')
-                                     },
-                                 'specials' =>
-                                     {
-                                         'items' => 'no items'
-                                     }
-
-                                })
+                                  })
+    end
   end
 end
+
--- spec/requests/api/v1/session_spec.rb
+++ spec/requests/api/v1/session_spec.rb
@@ -1,63 +1,65 @@
 require 'rails_helper'
 
+describe Api::V1::SessionsController do
+  describe 'POST /v1/sessions' do
 
-describe 'GET /v1/sessions' do
+    before do
+      @admin = FactoryGirl.create(:user, is_admin: true)
+      @user = FactoryGirl.create(:user)
+    end
 
-  before do
-    @admin = FactoryGirl.create(:user, is_admin: true)
-    @user = FactoryGirl.create(:user)
+    describe 'admin login' do
+      it 'valid credentials returns user & token' do
+        post '/v1/sessions', "email=#{@admin.email}&password=#{@admin.password}"
+        expect(response_json).to eq({'user' => {
+                                        'email' => @admin.email,
+                                        'is_admin' => "#{@admin.is_admin}"
+                                    },
+                                     'authentication_token' => {
+                                         'token' => @admin.authentication_token
+                                     }
+                                    })
+      end
+
+      it 'invalid password returns error message' do
+        post '/v1/sessions', "email=#{@admin.email}&password=bogus"
+        expect(response_json).to eq({ 'message' => 'Error with your login or password' })
+        expect(response.status).to eq 401
+      end
+
+      it 'invalid email returns error message' do
+        post '/v1/sessions', "email=bogus&password=#{@admin.password}"
+        expect(response_json).to eq({ 'message' => 'Error with your login or password' })
+        expect(response.status).to eq 401
+      end
+
+    end
+
+    describe 'client login' do
+      it 'valid credentials returns user & token' do
+        post '/v1/sessions', "email=#{@user.email}&password=#{@user.password}"
+        expect(response_json).to eq({'user' => {
+                                        'email' => @user.email
+                                    },
+                                     'authentication_token' => {
+                                         'token' => @user.authentication_token
+                                     }
+                                    })
+      end
+
+      it 'invalid password returns error message' do
+        post '/v1/sessions', "email=#{@user.email}&password=bogus"
+        expect(response_json).to eq({ 'message' => 'Error with your login or password' })
+        expect(response.status).to eq 401
+      end
+
+      it 'invalid email returns error message' do
+        post '/v1/sessions', "email=bogus&password=#{@user.password}"
+        expect(response_json).to eq({ 'message' => 'Error with your login or password' })
+        expect(response.status).to eq 401
+      end
+
+    end
   end
 
-  describe 'admin login' do
-    it 'valid credentials returns user & token' do
-      post '/v1/sessions', "email=#{@admin.email}&password=#{@admin.password}"
-      expect(response_json).to eq({'user' => {
-                                      'email' => @admin.email,
-                                      'is_admin' => "#{@admin.is_admin}"
-                                  },
-                                   'authentication_token' => {
-                                       'token' => @admin.authentication_token
-                                   }
-                                  })
-    end
-
-    it 'invalid password returns error message' do
-      post '/v1/sessions', "email=#{@admin.email}&password=bogus"
-      expect(response_json).to eq({ 'message' => 'Error with your login or password' })
-      expect(response.status).to eq 401
-    end
-
-    it 'invalid email returns error message' do
-      post '/v1/sessions', "email=bogus&password=#{@admin.password}"
-      expect(response_json).to eq({ 'message' => 'Error with your login or password' })
-      expect(response.status).to eq 401
-    end
-
-  end
-
-  describe 'client login' do
-    it 'valid credentials returns user & token' do
-      post '/v1/sessions', "email=#{@user.email}&password=#{@user.password}"
-      expect(response_json).to eq({'user' => {
-                                      'email' => @user.email
-                                  },
-                                   'authentication_token' => {
-                                       'token' => @user.authentication_token
-                                   }
-                                  })
-    end
-
-    it 'invalid password returns error message' do
-      post '/v1/sessions', "email=#{@user.email}&password=bogus"
-      expect(response_json).to eq({ 'message' => 'Error with your login or password' })
-      expect(response.status).to eq 401
-    end
-
-    it 'invalid email returns error message' do
-      post '/v1/sessions', "email=bogus&password=#{@user.password}"
-      expect(response_json).to eq({ 'message' => 'Error with your login or password' })
-      expect(response.status).to eq 401
-    end
-
-  end
 end
