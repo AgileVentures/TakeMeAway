17c13cf5ff9c560d9da91d4db5258888032ac5c3
Sampriti Panda <sampdingo@hotmail.com>
2015-05-01 11:24:24 +0530

    Fix some more wrong associations, and uncomment some useful tests

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,8 +1,7 @@
 class MenuItem < ActiveRecord::Base
   has_one :image
   has_and_belongs_to_many :orders
-  has_many :menu_categories
-  has_many :menus, through: :menu_categories
+  has_and_belongs_to_many :menu_categories
 
   validates :name, presence: true
   validates :price, presence: true
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ActiveRecord::Base
   has_and_belongs_to_many :menu_items
   belongs_to :user
 
-  validates_presence_of :user_id
+  validates_presence_of :user
 
   validates_date :pickup_time, on_or_after: lambda { Date.current }
 end
