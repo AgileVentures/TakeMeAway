612d356f26812857631315b101bd1bea1ecfde8a
tochman <thomas@nocebo.se>
2015-05-03 15:40:58 +0200

    Menu model_spec is passing after refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503070839) do
+ActiveRecord::Schema.define(version: 20150503133704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20150503070839) do
     t.datetime "end_date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "title"
   end
 
   create_table "orders", force: :cascade do |t|
--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :menu do
     show_category true
+    title Faker::Name.name
     start_date "2015-04-28 20:07:53"
     end_date "2015-04-28 20:07:53"
   end
