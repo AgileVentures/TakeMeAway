557e157932f0fcae4caca3e07239d547a48fef7a
tochman <thomas@nocebo.se>
2015-05-04 21:44:34 +0200

    removed AV link from menu

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -199,11 +199,11 @@ ActiveAdmin.setup do |config|
   #
   # If you wanted to add a static menu item to the default menu provided:
   #
-    config.namespace :admin do |admin|
-      admin.build_menu :default do |menu|
-        menu.add label: 'AgileVentures', url: 'http://www.agileventures.org', html_options: { target: :blank }, priority: 0
-      end
-    end
+    # config.namespace :admin do |admin|
+    #   admin.build_menu :default do |menu|
+    #     menu.add label: 'AgileVentures', url: 'http://www.agileventures.org', html_options: { target: :blank }, priority: 0
+    #   end
+    # end
 
   # == Download Links
   #
