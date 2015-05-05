27aa1c296bf463d98754a14bbfce88461192f015
tochman <thomas@nocebo.se>
2015-05-05 09:44:42 +0200

    added specs for POST request together vith controller methods and jbuilder views

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::OrdersController < ApiController
+
+  # t.integer  "user_id"
+  # t.string   "status"
+  # t.datetime "order_time"
+  # t.datetime "pickup_time"
+  # t.datetime "fulfillment_time"
+  # t.datetime "created_at",       null: false
+  # t.datetime "updated_at",       null: false
+
+  def create
+    attributes = order_params
+    binding.pry
+    @order = Order.create(attributes)
+    invalid_request unless @order.save
+  end
+
+
+  def update
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def invalid_request
+    render json: {message: 'Error'}, status: 401
+  end
+
+  def order_params
+    json_params = ActionController::Parameters.new(JSON.parse(request.body.read))
+    json_params.require(:order).permit(:user_id, :status, :order_time, :pickup_time)
+  end
+
+
+end
--- spec/requests/api/v1/order_spec.rb
+++ spec/requests/api/v1/order_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 describe Api::V1::OrdersController do
+
+  # t.integer  "user_id"
+  # t.string   "status"
+  # t.datetime "order_time"
+  # t.datetime "pickup_time"
+  # t.datetime "fulfillment_time"
+  # t.datetime "created_at",       null: false
+  # t.datetime "updated_at",       null: false
+
+  let!(:user) { FactoryGirl.create(:user) }
+
   describe 'POST /v1/orders' do
+    before do
+      params = {order: {user_id: user.id, status: 'pending', order_time: Time.zone.now, pickup_time: Time.zone.now + 1.hour}}
+      post '/v1/orders', params.to_json
+    end
+
+    it 'creates an Order instance' do
+      expect(response_json).to eq({'instance' =>
+                                       {
+                                           'user_id' => user.id
+                                       }
+                                  })
+    end
 
   end
 
