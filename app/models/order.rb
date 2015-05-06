7789e9822539e16202efd40b7af4024155a0f72d
tochman <thomas@nocebo.se>
2015-05-06 11:05:22 +0200

    added custom message to validator, refactored fixture and spec to match new conditions

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ActiveRecord::Base
   validates_presence_of :user
 
   #validates :pickup_time, on_or_after: lambda { Date.current }
-  #PATCHING with validator frpm 'date_validator'
+  #PATCHING with validator from 'date_validator' with a quick fix for validation message
 
-  validates :pickup_time, date: { after: Proc.new { Time.now }}
+  validates :pickup_time, date: { after: Proc.new { Time.now }, message: '%{value} didn\'t pass validation'}
 end
