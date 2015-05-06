a1dd44c555c2fe9fc195c2f34309cee1cd9233d3
tochman <thomas@nocebo.se>
2015-05-06 10:35:25 +0200

    PATCH added 'date_validator' and refactored 'pickup_time' validation

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'devise'
 gem 'activeadmin', github: 'activeadmin'
 gem 'cancancan', '~> 1.10.1'
 gem 'active_admin_datetimepicker'
+gem 'date_validator'
 
 group :development do
   # gem "spring"
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,8 @@ class Order < ActiveRecord::Base
 
   validates_presence_of :user
 
-  validates :pickup_time, on_or_after: lambda { Date.current }
+  #validates :pickup_time, on_or_after: lambda { Date.current }
+  #PATCHING with validator frpm 'date_validator'
+
+  validates :pickup_time, date: { after: Proc.new { Time.now }}
 end
