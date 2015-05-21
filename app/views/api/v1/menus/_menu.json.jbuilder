93e2b8694842533d822128cce10aab5a4a4a137c
Raoul DIFFOUO <diraulo@gmail.com>
2015-05-21 16:11:49 +0200

    Refactor menu endpoint JSON response

--- app/views/api/v1/menus/_menu.json.jbuilder
+++ app/views/api/v1/menus/_menu.json.jbuilder
@@ -1,17 +1,17 @@
 json.cache! menu do
-  json.menu do
-    json.id menu.id
-    json.title menu.title
-    json.start_date menu.start_date.strftime('%F')
-    json.end_date menu.end_date.strftime('%F')
-  end
+  # json.menu do
+  json.id menu.id
+  json.title menu.title
+  json.start_date menu.start_date.strftime('%F')
+  json.end_date menu.end_date.strftime('%F')
+  # end
   json.uri url_for("#{request.protocol}#{request.host_with_port}#{Rails.application.routes.url_helpers.v1_menu_path(menu.id)}")
   json.item_count menu.menu_items.count
 
-
   if menu.menu_items
     json.items menu.menu_items do |item|
-      json.item item.name
+      json.id item.id
+      json.name item.name
       json.price item.price
       json.description item.description
       json.ingredients item.ingredients
@@ -19,6 +19,4 @@ json.cache! menu do
   else
     json.items '"no items"'
   end
-
-
 end
--- spec/requests/api/v1/menu_spec.rb
+++ spec/requests/api/v1/menu_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe Api::V1::MenusController do
   describe 'GET /v1/menus/:id' do
-
     before do
       Timecop.freeze(Date.parse('2015-05-06').at_beginning_of_week)
       @menus = FactoryGirl.create_list(:menu, 3)
@@ -19,134 +18,132 @@ describe Api::V1::MenusController do
     end
 
     it 'returns a menu index' do
-      get "/v1/menus"
-      expect(response_json).to eq({'date_range' => '2015-05-04..2015-05-10',
-                                   'menus' =>
-                                       [
-                                           {'menu' =>
-                                                {'id' => @menus[0].id,
-                                                 'title' => @menus[0].title,
-                                                 'start_date' => @menus[0].start_date.strftime('%F'),
-                                                 'end_date' => @menus[0].end_date.strftime('%F')},
-                                            'uri' => "http://www.example.com/v1/menus/#{@menus[0].id}",
-                                            'item_count' => 3,
-                                            'items' =>
-                                                [
-                                                    {
-                                                        'item' => @menu_items[0].name,
-                                                        'price' => @menu_items[0].price.to_f.to_s,
-                                                        'description' => @menu_items[0].description,
-                                                        'ingredients' => @menu_items[0].ingredients
-                                                    },
-                                                    {
-                                                        'item' => @menu_items[1].name,
-                                                        'price' => @menu_items[1].price.to_f.to_s,
-                                                        'description' => @menu_items[1].description,
-                                                        'ingredients' => @menu_items[1].ingredients
-                                                    },
-                                                    {
-                                                        'item' => @menu_items[2].name,
-                                                        'price' => @menu_items[2].price.to_f.to_s,
-                                                        'description' => @menu_items[2].description,
-                                                        'ingredients' => @menu_items[2].ingredients
-                                                    }
-                                                ]
-                                           },
-                                           {'menu' =>
-                                                {'id' => @menus[1].id,
-                                                 'title' => @menus[1].title,
-                                                 'start_date' => @menus[1].start_date.strftime('%F'),
-                                                 'end_date' => @menus[1].end_date.strftime('%F')},
-                                            'uri' => "http://www.example.com/v1/menus/#{@menus[1].id}",
-                                            'item_count' => 3,
-                                            'items' =>
-                                                [
-                                                    {
-                                                        'item' => @menu_items[0].name,
-                                                        'price' => @menu_items[0].price.to_f.to_s,
-                                                        'description' => @menu_items[0].description,
-                                                        'ingredients' => @menu_items[0].ingredients
-                                                    },
-                                                    {
-                                                        'item' => @menu_items[1].name,
-                                                        'price' => @menu_items[1].price.to_f.to_s,
-                                                        'description' => @menu_items[1].description,
-                                                        'ingredients' => @menu_items[1].ingredients
-                                                    },
-                                                    {
-                                                        'item' => @menu_items[2].name,
-                                                        'price' => @menu_items[2].price.to_f.to_s,
-                                                        'description' => @menu_items[2].description,
-                                                        'ingredients' => @menu_items[2].ingredients
-                                                    }
-                                                ]
-                                           },
-                                           {'menu' =>
-                                                {'id' => @menus[2].id,
-                                                 'title' => @menus[2].title,
-                                                 'start_date' => @menus[2].start_date.strftime('%F'),
-                                                 'end_date' => @menus[2].end_date.strftime('%F')},
-                                            'uri' => "http://www.example.com/v1/menus/#{@menus[2].id}",
-                                            'item_count' => 3,
-                                            'items' =>
-                                                [
-                                                    {
-                                                        'item' => @menu_items[0].name,
-                                                        'price' => @menu_items[0].price.to_f.to_s,
-                                                        'description' => @menu_items[0].description,
-                                                        'ingredients' => @menu_items[0].ingredients
-                                                    },
-                                                    {
-                                                        'item' => @menu_items[1].name,
-                                                        'price' => @menu_items[1].price.to_f.to_s,
-                                                        'description' => @menu_items[1].description,
-                                                        'ingredients' => @menu_items[1].ingredients
-                                                    },
-                                                    {
-                                                        'item' => @menu_items[2].name,
-                                                        'price' => @menu_items[2].price.to_f.to_s,
-                                                        'description' => @menu_items[2].description,
-                                                        'ingredients' => @menu_items[2].ingredients
-                                                    }
-                                                ]
-                                           }
-                                       ]
-                                  })
+      get '/v1/menus'
+      expect(response_json).to eq('date_range' => '2015-05-04..2015-05-10',
+                                  'menus' => [{
+                                    'id' => @menus[0].id,
+                                    'title' => @menus[0].title,
+                                    'start_date' => @menus[0].start_date.strftime('%F'),
+                                    'end_date' => @menus[0].end_date.strftime('%F'),
+                                    'uri' => "http://www.example.com/v1/menus/#{@menus[0].id}",
+                                    'item_count' => 3,
+                                    'items' => [
+                                      {
+                                        'id' => @menu_items[0].id,
+                                        'name' => @menu_items[0].name,
+                                        'price' => @menu_items[0].price.to_f.to_s,
+                                        'description' => @menu_items[0].description,
+                                        'ingredients' => @menu_items[0].ingredients
+                                      },
+                                      {
+                                        'id' => @menu_items[1].id,
+                                        'name' => @menu_items[1].name,
+                                        'price' => @menu_items[1].price.to_f.to_s,
+                                        'description' => @menu_items[1].description,
+                                        'ingredients' => @menu_items[1].ingredients
+                                      },
+                                      {
+                                        'id' => @menu_items[2].id,
+                                        'name' => @menu_items[2].name,
+                                        'price' => @menu_items[2].price.to_f.to_s,
+                                        'description' => @menu_items[2].description,
+                                        'ingredients' => @menu_items[2].ingredients
+                                      }
+                                    ]
+                                  }, {
+                                    'id' => @menus[1].id,
+                                    'title' => @menus[1].title,
+                                    'start_date' => @menus[1].start_date.strftime('%F'),
+                                    'end_date' => @menus[1].end_date.strftime('%F'),
+                                    'uri' => "http://www.example.com/v1/menus/#{@menus[1].id}",
+                                    'item_count' => 3,
+                                    'items' => [
+                                      {
+                                        'id' => @menu_items[0].id,
+                                        'name' => @menu_items[0].name,
+                                        'price' => @menu_items[0].price.to_f.to_s,
+                                        'description' => @menu_items[0].description,
+                                        'ingredients' => @menu_items[0].ingredients
+                                      },
+                                      {
+                                        'id' => @menu_items[1].id,
+                                        'name' => @menu_items[1].name,
+                                        'price' => @menu_items[1].price.to_f.to_s,
+                                        'description' => @menu_items[1].description,
+                                        'ingredients' => @menu_items[1].ingredients
+                                      },
+                                      {
+                                        'id' => @menu_items[2].id,
+                                        'name' => @menu_items[2].name,
+                                        'price' => @menu_items[2].price.to_f.to_s,
+                                        'description' => @menu_items[2].description,
+                                        'ingredients' => @menu_items[2].ingredients
+                                      }
+                                    ]
+                                  }, {
+                                    'id' => @menus[2].id,
+                                    'title' => @menus[2].title,
+                                    'start_date' => @menus[2].start_date.strftime('%F'),
+                                    'end_date' => @menus[2].end_date.strftime('%F'),
+                                    'uri' => "http://www.example.com/v1/menus/#{@menus[2].id}",
+                                    'item_count' => 3,
+                                    'items' => [
+                                      {
+                                        'id' => @menu_items[0].id,
+                                        'name' => @menu_items[0].name,
+                                        'price' => @menu_items[0].price.to_f.to_s,
+                                        'description' => @menu_items[0].description,
+                                        'ingredients' => @menu_items[0].ingredients
+                                      },
+                                      {
+                                        'id' => @menu_items[1].id,
+                                        'name' => @menu_items[1].name,
+                                        'price' => @menu_items[1].price.to_f.to_s,
+                                        'description' => @menu_items[1].description,
+                                        'ingredients' => @menu_items[1].ingredients
+                                      },
+                                      {
+                                        'id' => @menu_items[2].id,
+                                        'name' => @menu_items[2].name,
+                                        'price' => @menu_items[2].price.to_f.to_s,
+                                        'description' => @menu_items[2].description,
+                                        'ingredients' => @menu_items[2].ingredients
+                                      }
+                                    ]
+                                  }])
     end
 
     it 'returns a menu by :id' do
       menu = @menus[0]
       get "/v1/menus/#{menu.id}"
-      expect(response_json).to eq({'menu' =>
-                                       {'id' => @menus[0].id,
-                                        'title' => @menus[0].title,
-                                        'start_date' => @menus[0].start_date.strftime('%F'),
-                                        'end_date' => @menus[0].end_date.strftime('%F')},
-                                   'uri' => "http://www.example.com/v1/menus/#{@menus[0].id}",
-                                   'item_count' => 3,
-                                   'items' =>
-                                       [
-                                           {
-                                               'item' => @menu_items[0].name,
-                                               'price' => @menu_items[0].price.to_f.to_s,
-                                               'description' => @menu_items[0].description,
-                                               'ingredients' => @menu_items[0].ingredients
-                                           },
-                                           {
-                                               'item' => @menu_items[1].name,
-                                               'price' => @menu_items[1].price.to_f.to_s,
-                                               'description' => @menu_items[1].description,
-                                               'ingredients' => @menu_items[1].ingredients
-                                           },
-                                           {
-                                               'item' => @menu_items[2].name,
-                                               'price' => @menu_items[2].price.to_f.to_s,
-                                               'description' => @menu_items[2].description,
-                                               'ingredients' => @menu_items[2].ingredients
-                                           }
-                                       ]
-                                  })
+      expect(response_json).to eq('id' => @menus[0].id,
+                                  'title' => @menus[0].title,
+                                  'start_date' => @menus[0].start_date.strftime('%F'),
+                                  'end_date' => @menus[0].end_date.strftime('%F'),
+                                  'uri' => "http://www.example.com/v1/menus/#{@menus[0].id}",
+                                  'item_count' => 3,
+                                  'items' => [
+                                    {
+                                      'id' => @menu_items[0].id,
+                                      'name' => @menu_items[2].name,
+                                      'price' => @menu_items[0].price.to_f.to_s,
+                                      'description' => @menu_items[0].description,
+                                      'ingredients' => @menu_items[0].ingredients
+                                    }, {
+                                      'id' => @menu_items[1].id,
+                                      'name' => @menu_items[1].name,
+                                      'price' => @menu_items[1].price.to_f.to_s,
+                                      'description' => @menu_items[1].description,
+                                      'ingredients' => @menu_items[1].ingredients
+                                    }, {
+                                      'id' => @menu_items[2].id,
+                                      'name' => @menu_items[2].name,
+                                      'price' => @menu_items[2].price.to_f.to_s,
+                                      'description' => @menu_items[2].description,
+                                      'ingredients' => @menu_items[2].ingredients
+                                    }
+                                  ]
+                                 )
     end
   end
 end
-
