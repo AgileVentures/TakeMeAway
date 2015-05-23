1708e7539eb1ff255e21e10140262b845cff7bc2
Sampriti Panda <sampdingo@hotmail.com>
2015-05-23 15:48:50 +0530

    Added comment about database storage

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,8 @@ class Image < ActiveRecord::Base
   belongs_to :menu_item
 
   has_attached_file :file,
-                    :storage => :database,
+                    # uncomment for database storage, which has problems with image routing
+                    # :storage => :database,
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
                     :default_url => "/images/:style/missing.png"
 
