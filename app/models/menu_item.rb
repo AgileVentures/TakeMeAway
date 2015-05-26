25194ee7e8fdbcc17fec1a5d98c22ede6fce2037
tochman <thomas@nocebo.se>
2015-05-26 13:01:33 +0200

    cleaning our old associations..

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,6 @@
 class MenuItem < ActiveRecord::Base
   has_one :image
-  has_and_belongs_to_many :orders
+  #has_and_belongs_to_many :orders
   has_and_belongs_to_many :menu_categories
   has_and_belongs_to_many :menus
 
