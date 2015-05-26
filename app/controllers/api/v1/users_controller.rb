30b7e626b6a3e55252d368c92a4245c13c106bfc
Razor <razorcd@yahoo.com>
2015-05-26 16:05:05 +0300

    password_confirmation is always required

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,7 @@ class Api::V1::UsersController < ApiController
 private
 
   def user_params
+    params[:password_confirmation] ||= ""
     params.permit(:name, :email, :password, :password_confirmation)
   end
 end
