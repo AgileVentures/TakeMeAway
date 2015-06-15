055daa96151ae316b9e397118a78c12960b5d4b3
Patrick Bolger <patmbolger@gmail.com>
2015-06-15 09:55:21 -0400

    added acceptance tests for AA changes associated with email notifications

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -15,6 +15,10 @@ def path_to(page_name, id = '')
       admin_menus_path
     when 'orders' then
       admin_orders_path
+    when 'admins' then
+      admin_admins_path
+    when 'edit admin'then
+      edit_admin_admin_path
     else
       raise('path to specified is not listed in #path_to')
   end
@@ -29,6 +33,15 @@ Given /^I am on the "([^"]*)" page$/ do |page|
   visit path_to(page)
 end
 
+Then(/^I should be on the edit page for user "([^"]*)"$/) do |user_name|
+  user = User.find_by(name: user_name)
+  expect(current_path).to eq edit_admin_admin_path(user)
+end
+
+Then(/^I should be on the view page for user "([^"]*)"$/) do |user_name|
+  user = User.find_by(name: user_name)
+  expect(current_path).to eq admin_admin_path(user)
+end
 
 Then(/^I should be on the edit page for Menu Item "([^"]*)"$/) do |item|
   product = MenuItem.find_by(name: item)
